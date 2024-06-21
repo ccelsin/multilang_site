c408c79c7f79fb4e5ac0f7a79060e9c55a7dce4d
Maodan Errol C. ADJAGBONI <maodan.adjagboni@groupe-esigelec.org>
2024-06-22 01:29:18 +0200

    Création du modèle pour gérer des articles de blog et création d'une vue pour afficher une liste d'articles.

--- main/models.py
+++ main/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+from django.utils import timezone
+
+#Création du modèle pour gérer des articles de blog avec les champs title, content, et publication_date.
+
+class Post(models.Model):
+    title = models.CharField(max_length=80)
+    content = models.CharField(max_length=255)
+    publication_date = models.DateField(auto_now_add=True)
--- main/views.py
+++ main/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from .models import Post
 
-# Create your views here.
+#Vue qui gère les requêtes HTTP pour la page d'accueil de l'application main
+def index(request):
+    context={
+        "posts": Post.objects.all()
+    }
+    return HttpResponse(render(request, "main/index.html", context))
