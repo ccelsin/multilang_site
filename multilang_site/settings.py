7235138710f561d8c9c874bed2bb5c190cc2793a
Maodan Errol C. ADJAGBONI <maodan.adjagboni@groupe-esigelec.org>
2024-06-30 05:22:08 +0200

    Adaptation au déploiement vercel

--- multilang_site/settings.py
+++ multilang_site/settings.py
@@ -12,14 +12,11 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 
 from pathlib import Path
 import os
-import environ
-import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-env = environ.Env()
-environ.Env.read.env()
+
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
@@ -33,7 +30,7 @@ LOCALE_PATHS = [
     os.path.join(BASE_DIR,'locale')
 ]
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.vercel.app']
 
 
 # Application definition
@@ -94,18 +91,13 @@ WSGI_APPLICATION = 'multilang_site.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
-"""
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-"""
 
-DATABASES = {
-    'default': dj_database_url.parse(env('DATABASE_URL'))
-}
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
