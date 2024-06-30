ede8a49c7406801c49e02c59de72882f5730a478
Maodan Errol C. ADJAGBONI <maodan.adjagboni@groupe-esigelec.org>
2024-06-30 21:04:13 +0200

    up

--- multilang_site/settings.py
+++ multilang_site/settings.py
@@ -33,7 +33,7 @@ LOCALE_PATHS = [
     os.path.join(BASE_DIR,'locale')
 ]
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.vercel.app']
 
 
 # Application definition
@@ -94,18 +94,15 @@ WSGI_APPLICATION = 'multilang_site.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
-"""
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-"""
 
-DATABASES = {
-    'default': dj_database_url.parse(env('DATABASE_URL'))
-}
+
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
